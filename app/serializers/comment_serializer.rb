726ee99a31775fb7b787004799a4727b86ed59b2
wwweert123 <puachengxuan.fp@gmail.com>
2024-01-15 16:08:03 +0800

    Add missing weighted score field

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :text, :author, :forum_thread_id, :created_at, :forum_thread
+  attributes :text, :author, :forum_thread_id, :created_at, :forum_thread, :cached_weighted_score
 
   belongs_to :author
 end
